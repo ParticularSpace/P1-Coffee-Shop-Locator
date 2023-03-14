136ad9034ba9864172582b3167ab6bfea3627c03
Sam Jones <samejones2018@gmail.com>
2023-03-13 21:25:16 -0700

    added css and button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
-zipCodeBtn = $('#zip-code-btn');
+zipCodeBtn = $('.primary-btn');
 zipDataDisplay = $('.zip-data');
 
 
 zipCodeBtn.on('click', function() {
     var zipCode = $('#zip-code-btn').val();
     if (zipCode.length !== 5) {
+        $('#zip-code-btn').val('');
         alert('Please enter a valid zip code');
     } else {
         zipDataDisplay.textContent = 'Loading...';
