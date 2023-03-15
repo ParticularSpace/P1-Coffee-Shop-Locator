9f1670eac54b0d31dd920dbd9e33385047581c10
Sam Jones <samejones2018@gmail.com>
2023-03-14 20:37:00 -0700

    added api for coffee

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,21 @@
-Document.ready(function() {
+$(document).ready(function() {
 
 zipCodeBtn = $('.primary-btn');
 zipDataDisplay = $('.zip-data');
 
+const coffeeAPI = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://the-coffee-api.p.rapidapi.com/drinks/unique/id/1?keys=description%2ChasAlcohol%2CcupSize%2Crecipe",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "533ff7e0a9msh3eba0022d84c638p19ec67jsnb6f2f7fce0a0",
+		"X-RapidAPI-Host": "the-coffee-api.p.rapidapi.com"
+	}
+};
 
 zipCodeBtn.on('click', function() {
+    console.log(coffeeAPI)
     var zipCode = $('#zip-code-btn').val();
     if (zipCode.length !== 5) {
         $('#zip-code-btn').val('');
@@ -12,7 +23,7 @@ zipCodeBtn.on('click', function() {
     } else {
         //Locator API
         $.ajax({
-            url: ' ',
+            url: 'https://the-coffee-api.p.rapidapi.com/drinks/unique/id/1',
             dataType: 'json',
             success: function(data) {
                 var zipData = data;
@@ -31,4 +42,4 @@ zipCodeBtn.on('click', function() {
     }
 });
 
-
+});
