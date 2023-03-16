82c15eb3f84c709c803f18436b2c28baa1763fb4
escotoj <j.escoto14@gmail.com>
2023-03-15 20:42:02 -0700

    add button function on js and html framework for quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 zipCodeBtn = $('.primary-btn');
 zipDataDisplay = $('.zip-data');
 
+// startQuiz = $('#startQuiz');
+// questionPromp = $('#questionPromp');
+// answers = $('.answers')
+
+
 
 zipCodeBtn.on('click', function() {
     var zipCode = $('#zip-code-btn').val();
@@ -10,4 +15,10 @@ zipCodeBtn.on('click', function() {
     } else {
         zipDataDisplay.textContent = 'Loading...';
     }
+});
+
+
+// function for when we click on start quiz, upon clicking the button, the quiz is rendered. so far the alert box keeps popping up when i click this button.
+startQuiz.on("click", function () {
+    console.log("container");
 });
