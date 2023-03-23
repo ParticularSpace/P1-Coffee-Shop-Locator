c44dc0b34c0f52dc25bb168de272e9431f08db3b
Sam Jones <samejones2018@gmail.com>
2023-03-23 12:18:06 -0700

    overhauled js for quiz

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,4 +1,4 @@
-const answers = document.querySelectorAll("answers");
+const answers = document.querySelectorAll("button.answer");
 const question = document.getElementById("questionPromp");
 const mainBox = document.getElementById("box");
 var choiceA = document.getElementById("choiceA");
@@ -6,25 +6,24 @@ var choiceB = document.getElementById("choiceB");
 var choiceC = document.getElementById("choiceC");
 var choiceD = document.getElementById("choiceD");
 var startQuiz = document.getElementById("startQuiz");
-// var startTimer = document.getElementById("startTimer");
 var correctAlert = document.getElementById("correctAlert");
 
 var startTime = 500;
 
 // TO-DO : have the answer buttons hide until start quiz is clicked, only when i click buttons not the box element
-// answers.classList.add("hide");
 
 startQuiz.addEventListener("click", function () {
   mainBox.classList.remove("hide");
+  startQuiz.classList.add("hide");
   renderQuestion();
 });
 
-// questions based around the data pulled from API, want to include coditional statements using boolean and array object in the data
+
 var questionPromp = [
   {
-    question: "Would you like milk or Half-and-half?",
-    choiceA: "Yes",
-    choiceB: "No",
+    question: "Coffee bitter or sweet?",
+    choiceA: "Bitter",
+    choiceB: "Sweet",
     choiceC: "",
     choiceD: "",
     correct: "choiceA",
@@ -32,13 +31,13 @@ var questionPromp = [
   {
     question: "Which one do you prefer?",
     choiceA: "Milk",
-    choiceB: "Half-and-half",
+    choiceB: "Non-dairy",
     choiceC: "",
     choiceD: "",
     correct: "choiceA",
   },
   {
-    question: "Would you like milk foam?",
+    question: "Would you like foam?",
     choiceA: "Yes",
     choiceB: "None for me",
     choiceC: "",
@@ -62,7 +61,7 @@ var questionPromp = [
     correct: "choiceB",
   },
   {
-    question: "Would you like to add a shot of alcohol?",
+    question: "Would you like to add a shot of espresso?",
     choiceA: "Please",
     choiceB: "Uh, no thanks",
     choiceC: "",
@@ -74,23 +73,20 @@ var questionPromp = [
 const lastQuestion = questionPromp.length - 1;
 
 var questionIndex = 0;
+var choices = [];
 
-console.log(question);
-
+// show the questions
 function renderQuestion() {
   var q = questionPromp[questionIndex];
   question.textContent = q.question;
   choiceA.innerHTML = q.choiceA;
   choiceB.innerHTML = q.choiceB;
-  // choiceC.innerHTML = q.choiceC;
-  // choiceD.innerHTML = q.choiceD;
 }
 
 var answersClick = document.querySelector("section");
 
 answersClick.addEventListener("click", function (event) {
   var buttonClickID = event.target.id;
-  console.log(buttonClickID);
   checkAnswer(buttonClickID);
 });
 
@@ -100,68 +96,71 @@ function checkAnswer(buttonClickID) {
   } else {
     correctAlert.textContent = "Nice!";
   }
-};
 
+  // add the user's choice to an array
+  choices.push(buttonClickID);
 
-// quiz end function so that drink is displayed, along with description and recepi for the drink
+  questionIndex++;
+
+  if (startTime === 0 || questionIndex === questionPromp.length) {
+    console.log("Quiz is over");
+    quizEnd();
+  } else {
+    renderQuestion();
+  }
+}
+
 function quizEnd() {
-  initials.classList.remove("hide");
-};
   
+  // assign a coffee type based on user choices
+  coffeeType = getCoffeeType(choices);
 
-  
+  //i want to set coffeeType to local storage
+  localStorage.setItem("coffeeType", coffeeType);
 
+  //api info for the coffee type
 
+  // hide the quiz section and display the initials input box
+  mainBox.classList.add("hide");
+}
 
+// function to assign a coffee type based on user choices
+function getCoffeeType(choices) {
+  // define the coffee types and their corresponding question numbers
+  var coffeeTypes = [
+    {name: "latte", questions: [0, 1, 2]},
+    {name: "cappuccino", questions: [3, 4, 5]},
+    {name: "iced coffee", questions: [1, 2, 4]},
+    {name: "espresso", questions: [0, 2, 5]},
+    {name: "macchiato", questions: [1, 3, 5]},
+  ];
 
+  // count the number of times each choice was selected
+  var counts = {choiceA: 0, choiceB: 0, choiceC: 0, choiceD: 0};
+  for (var i = 0; i < choices.length; i++) {
+    counts[choices[i]]++;
 
+  }
 
-
-
-
-
-// add a condition that gives a correct answer a point.
-  // var finalScore = localStorage.getItem("points");
-
-  // function correctScore(buttonClickID) {
-  //     if (questionPromp[questionIndex].correct === buttonClickID) {
-
-  //     } else {;
-  //     }
-
-  // questionIndex++;
-  // if (startTime === 0 || questionIndex === questionPromp.length) {
-  //  console.log("hello"); 
-  //  console.log(questionIndex)
-  //  console.log(questionPromp.length)
-  //   quizEnd();
-  // } else {
-  //   renderQuestion();
-  //   console.log("duck");
-  // };
- 
-
-// var nameBox = document.getElementById("answers");
-// function inputBox () {
-//   document.createElement("INPUT");
-// nameBox.setAttribute("type", "text");
-// };
-
-
-
-  // initials.classList.remove ("hide");
-  // mainBox.classList.add("hide");
-  // clearInterval(startQuiz);
-  // initials.classList.remove("initials");
-
-
-// startQuiz = $('#startQuiz');
-// questionPromp = $('#questionPromp');
-// answers = $('.answers')
-
-//   startQuiz.on("click", function () {
-//     console.log("container");
-// });
- 
+  // determine which coffee type has the most questions answered in its favor
+  var maxCount = 0;
+  var coffeeType = "";
+  for (var i = 0; i < coffeeTypes.length; i++) {
+    var typeCount = 0;
+    for (var j = 0; j < coffeeTypes[i].questions.length; j++) {
+      if (choices[coffeeTypes[i].questions[j]] === "choiceA") {
+        typeCount++;
+      } else {
+        typeCount--;
+      }
+    }
+    if (typeCount > maxCount) {
+      maxCount = typeCount;
+      coffeeType = coffeeTypes[i].name;
+    }
+  }
+  console.log(coffeeType);
+  return coffeeType;
+}
 
 
