ee46cd3acb2271c1dc683d698a3d6ecdcdab6b9b
Al Hernandez <awessonn@gmail.com>
2023-03-23 20:41:02 -0700

    linked quiz results to the recipe api and displayed it on index.html.

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -119,9 +119,10 @@ function quizEnd() {
   localStorage.setItem("coffeeType", coffeeType);
 
   //api info for the coffee type
-
+ 
   // hide the quiz section and display the initials input box
   mainBox.classList.add("hide");
+  window.location.href = "index.html";
 }
 
 // function to assign a coffee type based on user choices
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,158 @@
+let longi = '';
+let lati = '';
+
+let rec; // recipes
+let loc; // location
+//let loop = loc.length;
+
+let test = "flat_white";
+
+	// external variable: let x = value of quiz score.
+	let $x = localStorage.getItem("coffeeType"); // = quiz score;
+	// Coffee recipes
+
+	const coffeeRecipes = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://the-coffee-api.p.rapidapi.com/drinks/unique/name/" + $x + "?keys=description%2ChasAlcohol%2CcupSize%2Crecipe",
+		"method": "GET",
+		"headers": {
+			"X-RapidAPI-Key": "b89c6902dbmsh56ed3f58eeefa96p1b1bcejsn66fb37adad3c", 
+			"X-RapidAPI-Host": "the-coffee-api.p.rapidapi.com"
+		}
+	};
+
+	// This requests the data back from the coffee recipe API. 
+	$.ajax(coffeeRecipes).done(function (response) {
+		console.log(response);
+		rec = response;
+	  displayRecipes();
+	});
+
+
+
+
+	// displayRecipes does exactly that. It's target is index.html under the quiz container. 
+	function displayRecipes () {
+		let counter = rec.recipe.ingredients.length;
+		let counter2 = rec.recipe.instructions.length;
+		let pHolder1 = rec.name;
+		let tempArray = [];
+
+	
+	
+		for(j = 0; j < counter; j++){
+			tempArray.push(rec.recipe.ingredients[j]);
+		}
+	
+		let pHolder2 = tempArray.map(function(el){
+			return el.name + " " + el.quantity;
+		});
+
+		let pHolder3 = [];
+
+		for(k = 0; k < counter2; k++){
+			pHolder3.push(rec.recipe.instructions[k]);
+		}
+
+		let $newLi = $("<li/>")
+			.addClass("card")
+			.attr("style","background-color: lightblue; padding: 1%; border-radius: 12px; margin-top: 2%; list-style-type: none;transition: all 0.2s ease-in-out; background: linear-gradient(124deg, cadetblue, lightskyblue, lightsteelblue,lightblue, cornflowerblue); background-size: 1800% 1800%; -webkit-animation: rainbow 8s ease infinite;-z-animation: rainbow 8s ease infinite;-o-animation: rainbow 8s ease infinite;animation: rainbow 12s ease infinite;")
+			.html(pHolder1 + ": " + pHolder2.join(", ") + "... " + pHolder3.join(", "));
+		$(".recipe-container").append($newLi);
+
+	};
+
+// this function dynamically creates the html elements along with css attributes for the location api to display its results onto the page.
+function displayLocations () {
+
+	let $parent = $(".locations");
+
+	if($parent.children().length > 0){
+		$parent.empty();
+
+	}
+	
+	for(let i = 0; i < loc.length; i++){
+
+		let temp1 = loc[i].poi.name;
+		let temp2 = loc[i].address.streetNumber;
+		let temp3 = loc[i].address.streetName;
+		let temp4 = loc[i].poi.phone;
+		let $newLi = $("<li/>")
+		.addClass("card")
+		.attr("style","background-color: beige; padding: 1%; border-radius: 12px; margin-top: 2%; list-style-type: none")//;transition: all 0.2s ease-in-out; background: linear-gradient(124deg, cadetblue, lightskyblue, lightsteelblue,lightblue, cornflowerblue); background-size: 1800% 1800%; -webkit-animation: rainbow 8s ease infinite;-z-animation: rainbow 8s ease infinite;-o-animation: rainbow 8s ease infinite;animation: rainbow 12s ease infinite;
+		.html(temp1 + ", " + temp2 + " " + temp3 + ", " + temp4);
+
+	$(".locations").append($newLi);
+
+	}
+};
+
+$(document).ready(function () {
+	
+	// this function requests the "cafe" within city area in a 5000(feet?) radius.
+	function secondCall () {
+		
+		let searchApi = `https://api.tomtom.com/search/2/categorySearch/cafe.json?lat=${encodeURIComponent(lati)}&lon=${encodeURIComponent(longi)}&radius=5000&openingHours=nextSevenDays&key=FLQAGv9rGb43fsKGnXayxy5Tm0Au1azE`;
+		
+		console.log(searchApi,"Api");
+		fetch(searchApi)
+			.then(function(response){
+			return response.json();
+		})
+			.then(function(data){
+			console.log(data);
+			loc = data.results;
+
+			displayLocations();
+		});
+		
+	}
+
+	// this is the search button function that will call for the cafes.
+	$("#zip-code-btn").click(function(){
+		let $zipCodeBtn = $(".form-control").val()
+
+		let apiKey = "4f9b9f0d81395d4880a33e43a1783f9e";
+		//console.log($zipCodeBtn);
+    	let weatherURL = `https://api.openweathermap.org/data/2.5/forecast?q=${encodeURIComponent($zipCodeBtn)}&units=imperial&appid=${encodeURIComponent(apiKey)}`;
+
+	
+
+		fetch(weatherURL)
+        	.then(function(response){
+            return response.json();
+        })
+    	.then(function(data){
+        	//console.log(data);
+			//===========================
+			lati = data.city.coord.lat;
+			longi = data.city.coord.lon;
+			
+			//console.log(lati);
+			//console.log(longi);
+			secondCall(lati,longi);
+			
+		});
+	
+		
+		
+	});
+
+
+
+
+
+	
+});
+
+
+
+
+
+//=======================================================================================
+
 $(document).ready(function() {
 
 zipCodeBtn = $('#zip-code-btn');
@@ -5,70 +160,7 @@ zipDataDisplay = $('.zip-data');
 takeQuiz = $('.takeQuiz');
 coffeeQuiz = $('#centerQuiz');
 
-const coffeeAPI = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://the-coffee-api.p.rapidapi.com/drinks/unique/id/1?keys=description%2ChasAlcohol%2CcupSize%2Crecipe",
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": "533ff7e0a9msh3eba0022d84c638p19ec67jsnb6f2f7fce0a0",
-		"X-RapidAPI-Host": "the-coffee-api.p.rapidapi.com"
-	}
-};
 
-const restaurantNear = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://restaurants-near-me-usa.p.rapidapi.com/restaurants/location/state/MI/city/West%20Bloomfield/0",
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": "81ab8adb7dmsha1dccf88ead99f0p166e50jsn077cda1fb367",
-		"X-RapidAPI-Host": "restaurants-near-me-usa.p.rapidapi.com"
-	}
-};
-
-/*
-coffeeQuiz.on('click', function() {
-    console.log(restaurantNear)
-
-    
-});
-
-zipCodeBtn.on('click', function() {
-    console.log(coffeeAPI)
-
-
-
-
-
-
-    var zipCode = $('#zip-code-btn').val();
-    if (zipCode.length !== 5) {
-        $('#zip-code-btn').val('');
-        alert('Please enter a valid zip code');
-    } else {
-        //Locator API
-        $.ajax({
-            url: 'https://the-coffee-api.p.rapidapi.com/drinks/unique/id/1',
-            dataType: 'json',
-            success: function(data) {
-                var zipData = data;
-                zipDataDisplay.html(zipData);
-            }
-        });
-        //Ingredients API
-        $.ajax({
-            url: ' ',
-            dataType: 'json',
-            success: function(data) {
-                var zipData = data;
-                zipDataDisplay.html(zipData);
-            }
-        });
-    }
-});
-
-*/
 startQuiz = $('#startQuiz');
 questionPromp = $('#questionPromp');
 answers = $('.answers')
@@ -84,3 +176,4 @@ takeQuiz.on('click', function() {
 });
 });
 
+//======================================================================================
