e010c6b1dd2170fed601d888345a7c7ea8a3da83
Sam Jones <samejones2018@gmail.com>
2023-03-23 20:51:15 -0700

    pushing rest of code

--- assets/js/script.js
+++ assets/js/script.js
@@ -150,8 +150,18 @@ $(document).ready(function () {
 
 
 $(document).ready(function() {
-
-zipCodeBtn = $('#zip-code-btn');
-zipDataDisplay = $('.zip-data');
-takeQuiz = $('.takeQuiz');
-coffeeQuiz = $('#centerQuiz');
+	zipCodeBtn = $('#zip-code-btn');
+	zipDataDisplay = $('.zip-data');
+	takeQuiz = $('.takeQuiz');
+	coffeeQuiz = $('#centerQuiz');
+	startQuiz = $('#startQuiz');
+	questionPromp = $('#questionPromp');
+	answers = $('.answers')
+	  startQuiz.on("click", function () {
+		console.log("container");
+	});
+	takeQuiz.on('click', function() {
+		//take me to quiz.html
+		window.location.href = "quiz.html";
+	});
+	});
